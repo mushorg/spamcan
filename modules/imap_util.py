dd8065bd3e2f54540d72622345272d19863e8beb
glaslos <glaslos@gmail.com>
2013-01-09 21:54:25 +0100

    IMAP handler restructured

--- modules/imap_util.py
+++ modules/imap_util.py
@@ -1,19 +1,15 @@
 import imaplib
-import email
 
 
-def get_mails(user_name, password, hostname):
-    mail = imaplib.IMAP4_SSL('imap.gmail.com')
-    mail.login('myusername@gmail.com', 'mypassword')
-    mail.list()
-    mail.select("inbox")
+class IMAPUtil(object):
 
-    result, data = mail.uid('search', None, "ALL")
-    latest_email_uid = data[0].split()[-1]
-    result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-    raw_email = data[0][1]
-    email_message = email.message_from_string(raw_email)
+    def imap_connect(self, user_name, password, hostname):
+        self.mail = imaplib.IMAP4_SSL(hostname)
+        self.mail.login(user_name, password)
 
-    print email_message['To']
-    print email.utils.parseaddr(email_message['From'])
-    print email_message.items()
+    def __init__(self, user_name, password, hostname):
+        self.imap_connect(user_name, password, hostname)
+
+    def get_stats(self):
+        data = self.mail.select('INBOX')[1]
+        return int(data[0])
