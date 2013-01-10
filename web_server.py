968a91f80d755506c9d9738390a4a89af2c88a47
glaslos <glaslos@gmail.com>
2013-01-10 01:05:45 +0100

    example config filled data fetching move into server startup

--- web_server.py
+++ web_server.py
@@ -2,7 +2,7 @@ import json
 
 from wsgiref.simple_server import make_server
 
-from bottle import route, Bottle, static_file
+from bottle import Bottle, static_file
 from jinja2 import Environment, FileSystemLoader
 
 import database
@@ -11,6 +11,19 @@ from modules import imap_util
 
 app = Bottle()
 template_env = Environment(loader=FileSystemLoader("./templates"))
+
+db = database.Database()
+with open("accounts.json", "rb") as account_file:
+    for line in account_file:
+        if line.startswith("#"):
+            continue
+        account_config = json.loads(line)
+        db.add_account(account_config)
+accounts = db.fetch()
+imap_handler = imap_util.IMAPUtil(accounts[0].user_name,
+                                  accounts[0].password,
+                                  accounts[0].hostname)
+imap_count = imap_handler.get_stats()
 
 
 @app.route('/static/<filepath:path>')
@@ -25,17 +38,7 @@ def favicon():
 
 @app.route('/')
 def spamcan_handler():
-    db = database.Database()
-    with open("accounts.json", "rb") as account_file:
-        for line in account_file:
-            if line.startswith("#"):
-                continue
-            account_config = json.loads(line)
-            db.add_account(account_config)
-    accounts = db.fetch()
-    imap_handler = imap_util.IMAPUtil(accounts[0].user_name ,accounts[0].password ,accounts[0].hostname)
-    imap_count = imap_handler.get_stats()
-    template = template_env.get_template('spamcan.html')
+    template = template_env.get_template('index.html')
     return template.render(account_list=accounts, count=imap_count)
 
 
