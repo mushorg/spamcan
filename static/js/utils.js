1445600fe85cd933899815d0c5f7642f622da439
glaslos <glaslos@gmail.com>
2013-01-19 17:14:48 +0100

    indentation fixed

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,30 +1,30 @@
 $(function() {
-	    $('button#get_stats').bind('click', function() {
-	      $.ajax({
-                    type: 'POST',
-                    url: '/get_stats',
-                    data: {id: $(this).attr("value")},
-                    success: function(response) {
-                        $('#count').html(response);
-                    }
-                });
-	      return false;
-	    });
-	    $('button#delete_acc').bind('click', function() {
-	      var acc_id = $(this).attr("value");
-	      var r=confirm("Are you sure you want to delete account #" + acc_id + "?");
-	      if (r==true){
-		      $.ajax({
-	                    type: 'POST',
-	                    url: '/delete_acc',
-	                    data: {id: acc_id},
-	                    success: function(response) {
-	                        $('#notice').html(response);
-	                        $('tr#row' + acc_id).html('');
-	                    }
-	                });
-		  } else {
-	      		return false;
-	      }
-	    });
-	  });
+	$('button#get_stats').bind('click', function() {
+		$.ajax({
+			type: 'POST',
+			url: '/get_stats',
+			data: {id: $(this).attr("value")},
+			success: function(response) {
+				$('#count').html(response);
+			}
+		});
+		return false;
+	});
+	$('button#delete_acc').bind('click', function() {
+		var acc_id = $(this).attr("value");
+		var r=confirm("Are you sure you want to delete account #" + acc_id + "?");
+		if (r==true){
+			$.ajax({
+			type: 'POST',
+			url: '/delete_acc',
+			data: {id: acc_id},
+			success: function(response) {
+				$('#notice').html(response);
+				$('tr#row' + acc_id).html('');
+			}
+			});
+		} else {
+			return false;
+		}
+	});
+});
