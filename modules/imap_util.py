dabe3351a336dc9c87b092d1940767f253de12dd
glaslos <glaslos@gmail.com>
2013-01-20 01:28:31 +0100

    Fetch mail method added. Small typo fixed in maildir class.

--- modules/imap_util.py
+++ modules/imap_util.py
@@ -1,4 +1,5 @@
 import imaplib
+import email
 
 
 class IMAPUtil(object):
@@ -12,3 +13,11 @@ class IMAPUtil(object):
     def get_stats(self):
         data = self.mail.select('INBOX')[1]
         return int(data[0])
+
+    def fetch_mails(self):
+        _typ, data = self.mail.search(None, 'ALL')
+        for num in data[0].split():
+            _typ, msg_data = self.mail.fetch(num, '(RFC822)')
+            for response_part in msg_data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_string(response_part[1])
--- modules/maildir_utils.py
+++ modules/maildir_utils.py
@@ -6,7 +6,7 @@ class MaildirUtil(object):
     def __init__(self):
         pass
 
-    def creat_mailbox(self, dirname):
+    def create_mailbox(self, dirname):
         self.mbox = mailbox.Maildir(dirname)
 
     def add_mail(self, message):
