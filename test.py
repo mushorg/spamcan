5181f2b3e082c89eece2aa89acdf9251aecfb188
glaslos <glaslos@gmail.com>
2013-01-31 13:13:27 +0100

    Using a TCP socket client to test the POP server functionality

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import unittest
 import os
 import shutil
 import threading
-import time
+import socket
 
 import database
 from testing import pop_server
+from modules import mail_util
 
 
 class SpamCanTest(unittest.TestCase):
@@ -24,15 +25,24 @@ class SpamCanTest(unittest.TestCase):
 
     def test_database(self):
         self.db = database.Database()
+        # TODO: Extend
         self.assert_(1 == 1)
 
     def test_pop_server(self):
         server = pop_server.pop_server()
         t = threading.Thread(target=server.serve_forever)
         t.start()
-        time.sleep(3)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.connect(("localhost", 8088))
+            received = sock.recv(1024)
+            sock.sendall("QUIT foobar" + "\n")
+        finally:
+            sock.close()
         server.shutdown()
         t.join()
+        self.assert_(received == "+OK SpamCan test server ready" + "\r\n")
+
 
 if __name__ == "__main__":
     nose.run()
