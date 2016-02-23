2f78476bc96bfc56f934ef1628e11a137492a16e
zuBux <pariszoump@gmail.com>
2016-02-23 13:32:57 +0200

    Ported all existing functionality to ES

--- modules/mail_utils.py
+++ modules/mail_utils.py
@@ -1,9 +1,33 @@
 import mailbox
 import os
-
+from modules.protocols import imap_util, pop_util
 from email.parser import Parser
 
+#Handles POP3 and IMAP connections
+class MailUtil(object):
 
+    def __init__(self):
+        self.imap_handler = imap_util.IMAPUtil()
+        self.pop_handler = pop_util.POPUtil()
+
+    def request(self, account):
+        protocol_handler = None
+        if account.protocol == "imap":
+            self.imap_handler.imap_connect(account.user_name,
+                                      account.password,
+                                      account.hostname)
+            protocol_handler = self.imap_handler
+        elif account.protocol == "pop3":
+            try:
+                self.pop_handler.pop_connect(account.user_name,
+                                    account.password,
+                                    account.hostname)
+            except:
+                print "Unable to connect to %s" %account.hostname
+            protocol_handler = self.pop_handler
+        return protocol_handler
+
+#Handles mailboxes
 class MaildirUtil(object):
 
     def __init__(self):
